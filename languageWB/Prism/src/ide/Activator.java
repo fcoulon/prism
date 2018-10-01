6a3d08dc2a642fbeb4f11ae09c10b78f1d30a4b3
Fabien Coulon <fabien.coulon@obeo.fr>
2018-10-01 18:50:27 +0200

    Fix Prism crash when Rascal editor is opened at Eclipse startup

--- languageWB/Prism/src/ide/Activator.java
+++ languageWB/Prism/src/ide/Activator.java
@@ -1,8 +1,19 @@
 package ide;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -22,6 +33,8 @@ public class Activator extends AbstractUIPlugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
         
+        closeRascalEditor();
+        
         ClassLoader cl = this.getClass().getClassLoader();
         
         WorkspaceListener listener = getWorkspaceListener();
@@ -38,6 +51,25 @@ public class Activator extends AbstractUIPlugin {
         ResourcesPlugin.getWorkspace().addResourceChangeListener(listener);
         
         System.out.println("SLEBus started");
+    }
+    
+    private void closeRascalEditor() {
+    	IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page = win.getActivePage();
+		
+		Display.getDefault().syncExec(new Runnable() {
+            public void run() {
+				IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("TestIt");
+				if (project != null) {
+					IEditorInput input = new FileEditorInput(project.getFile("src/doors.mf"));
+					IEditorPart editor = page.findEditor(input);
+					if(editor != null) {
+						page.closeEditor(editor, true);
+					}
+				}
+            }
+         });
     }
     
     public static WorkspaceListener getWorkspaceListener() {
